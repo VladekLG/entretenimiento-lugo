7aa33d25e88e568af19d484def0316024d7798ee
vladeklg <leandrogabriel1998@hotmail.com>
2022-08-14 15:50:58 -0300

    Segundo commit para el desafio numero 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Mynavbar from "./components/Mynavbar/Mynavbar";
+/*Estilos propios */
+import "./components/Mynavbar/Mynavbar.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+      <Mynavbar></Mynavbar>
+      <h1>ArmorGamesVL<span>ReactJS</span></h1>
       </header>
     </div>
   );
